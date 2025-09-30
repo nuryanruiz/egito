5ad7d952f2c7c08b2b63b78dee567552f5af6388
nuryanruiz <nuryan.ruiz@escola.pr.gov.br>
2025-09-30 12:13:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+// Adiciona a classe 'ativo' ao primeiro passo ao carregar a página
+document.getElementById('passo-0').classList.add('ativo');
+
 const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-    button.addEventListener('click', function(){
+    button.addEventListener('click', function() {
         const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proxima');
 
+        // Remover a classe 'ativo' do passo atual
         atual.classList.remove('ativo');
+
+        // Adicionar a classe 'ativo' ao próximo passo
         document.getElementById(proximoPasso).classList.add('ativo');
-    })
-})
+    });
+});
